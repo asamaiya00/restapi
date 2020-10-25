189023f0390cb04aee5e454b7071c6d61eb9efe9
Animesh Samaiya <asamaiya00@gmail.com>
2020-10-25 20:46:58 +0530

    feat: mock data and GET request

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-// Book struct
+// Book model
 type Book struct {
 	ID     string  `json:"id"`
 	Isbn   string  `json:"isbn"`
@@ -15,14 +16,56 @@ type Book struct {
 	Author *Author `json:"author"`
 }
 
+// Author model
 type Author struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
 }
 
+// Book slice struct
+var books []Book
+
+// get all Books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// get Book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+// create Book
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// update Book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// delete Book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	// init router
 	r := mux.NewRouter()
+
+	books = append(books, Book{ID: "1", Isbn: "643423", Title: "Alchemist", Author: &Author{Firstname: "Paulo", Lastname: "Coelho"}})
+	books = append(books, Book{ID: "2", Isbn: "25533", Title: "The subtle art of not giving a f*ck", Author: &Author{Firstname: "Mark", Lastname: "Manson"}})
+	books = append(books, Book{ID: "3", Isbn: "123124", Title: "Who moved my cheese", Author: &Author{Firstname: "Spencer", Lastname: "Johnson"}})
 
 	// route endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
